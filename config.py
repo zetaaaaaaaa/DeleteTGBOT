3c90e81a6cb734bae7a87d66a0ee9ed11e3aa43f
zetaaaaaaaa <102250655+zetaaaaaaaa@users.noreply.github.com>
2022-09-30 09:23:03 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 api_id = '17897679'
 api_hash = 'e7681747482f5867af9de8171136a89b'
-bot_token = '5243754583:AAEquotPW9wkUrcala-ndBD1b06Sf4PRc-'
+bot_token = '5243754583:AAEquotPW9wkUrcala-ndBD1b06Sf4PRc-0'
